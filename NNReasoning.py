1fdd9b4966214a071bca3de515d8f0f1e9a99e1e
consciousgaze <consciousgaze@gmail.com>
2016-11-15 02:44:17 -0500

    Add. Add skeleton reasoning convolutional layers. But tensor dimensions needs to be fine tuned.

--- NNReasoning.py
+++ NNReasoning.py
@@ -11,26 +11,42 @@ from util import *
 def main():
     net = MemNet(10, 4, 20, 10, 0.3)
     w2i, i2w, train, test = input_data_parser('en', '1k', 19)
-    g = tf.Graph()
-    s = tf.Session(graph = g)
+    g = net.graph
+    s = net.sess
     fact = net.process_data(train[0])
     with s.as_default():
         with g.as_default():
             encoder = net.generate_encoder(6, np.asarray(fact[0]).shape)
+            results = []
             for idx, encodes in enumerate(encoder):
                 outputs, last_states, inputs = encodes
                 result = tf.contrib.learn.run_n({"outputs": outputs, "last_states": last_states},
                                                 n=1, feed_dict = {inputs: fact[idx]})
+                results.append(result)
+            # adding batch dimension
+            question = tf.constant(np.asarray([results[-1][-1]['outputs']]))
+            facts = []
+            for result in results[:-1]:
+                facts.append(tf.constant(np.asarray([result[-1]['outputs']])))
+            reasoner = net.generate_resaoner(question, facts)
+            print reasoner
 
 class MemNet():
-    def __init__(self, depth_dnn, depth_rnn, unit_size, num_reasoning, dropout):
+    def __init__(self, depth_cnn, depth_rnn, unit_size, num_reasoning, dropout):
         self.graph = tf.Graph()
         self.sess = tf.Session(graph = self.graph)
-        self.depth_dnn = depth_dnn
+        self.depth_cnn = depth_cnn
         self.depth_rnn = depth_rnn
         self.num_reasoning = num_reasoning
         self.dropout = dropout
         self.unit_size = unit_size
+
+        # init params
+        with self.sess.as_default():
+            with self.graph.as_default():
+                self.filter = tf.random_normal([1, 1, 20, 20], dtype = tf.float64)
+        self.strides = [1, 2, 2, 1]
+
 
     def process_data(self, train):
         f1 = []
@@ -76,6 +92,25 @@ class MemNet():
                                             )
                 sentence_holder.append((output, last_state, tmp))
         return sentence_holder
+
+    def generate_conv_layer(self, question, fact):
+        feed = tf.concat(0, [question, fact])
+        for i in range(self.depth_cnn):
+            with tf.variable_scope('depth_' + str(i)) as scope:
+                print feed.get_shape(), self.filter, self.strides
+                feed = tf.nn.conv2d(feed, filter = self.filter, strides = self.strides, padding = 'VALID')
+        return tf.split(0, 2, feed)
+
+    def generate_resaoner(self, question, facts):
+        qs = []
+        fs = []
+        for idx, fact in enumerate(facts):
+            with tf.variable_scope('fact_' + str(idx)) as scope:
+                q, f = self.generate_conv_layer(question, fact)
+                qs.append(q)
+                fs.append(f)
+        question = tf.concat(0, qs)
+        return (question, fs)
 
 if __name__ == '__main__':
     main()
