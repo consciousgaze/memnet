902c211c9c4ff5a85310db41a6e3b067bdb7cb5e
consciousgaze <consciousgaze@gmail.com>
2016-11-16 02:45:20 -0500

    Add. Add code that generates reasoning layers. Correcting tensor shape handling

--- NNReasoning.py
+++ NNReasoning.py
@@ -11,25 +11,10 @@ from util import *
 def main():
     net = MemNet(10, 4, 20, 10, 0.3)
     w2i, i2w, train, test = input_data_parser('en', '1k', 19)
-    g = net.graph
-    s = net.sess
     fact = net.process_data(train[0])
-    with s.as_default():
-        with g.as_default():
-            encoder = net.generate_encoder(6, np.asarray(fact[0]).shape)
-            results = []
-            for idx, encodes in enumerate(encoder):
-                outputs, last_states, inputs = encodes
-                result = tf.contrib.learn.run_n({"outputs": outputs, "last_states": last_states},
-                                                n=1, feed_dict = {inputs: fact[idx]})
-                results.append(result)
-            # adding batch dimension
-            question = tf.constant(np.asarray([results[-1][-1]['outputs']]))
-            facts = []
-            for result in results[:-1]:
-                facts.append(tf.constant(np.asarray([result[-1]['outputs']])))
-            reasoner = net.generate_resaoner(question, facts)
-            print reasoner
+    input_dimension = np.asarray(fact[0]).shape
+    print net.get_model(5, input_dimension)
+    return
 
 class MemNet():
     def __init__(self, depth_cnn, depth_rnn, unit_size, num_reasoning, dropout):
@@ -44,8 +29,8 @@ class MemNet():
         # init params
         with self.sess.as_default():
             with self.graph.as_default():
-                self.filter = tf.random_normal([1, 1, 20, 20], dtype = tf.float64)
-        self.strides = [1, 2, 2, 1]
+                self.filter = tf.random_normal([3, 1, 1, 1], dtype = tf.float64)
+        self.strides = [1, 1, 1, 1]
 
 
     def process_data(self, train):
@@ -64,8 +49,44 @@ class MemNet():
             q.append(d[5][:9])
         return f1, f2, f3, f4, f5, q
 
-    def generate_dnn_layer(self, encoded):
-        pass
+    def expand_sentence_tensor(self, sentence):
+        '''
+            convert sentence tensor to a 4D tensor that can be conved
+        '''
+        # currently a sentence is a 2d vector which is batch_size * sentence_rep_length
+        rlt = tf.expand_dims(sentence, 2)
+        rlt = tf.expand_dims(rlt, 3)
+        return rlt
+
+    def get_model(self, fact_num, input_dimension):
+        with self.sess.as_default():
+            with self.graph.as_default():
+                return self.generate_model(fact_num, input_dimension)
+
+    def generate_model(self, fact_num, input_dimension):
+        '''
+            fact_num is the number of fact sentences
+            input_dimension is:
+                batch * sentence length * word vector size
+        '''
+        encoder = self.generate_encoder(fact_num + 1, input_dimension)
+        inputs = []
+        facts = []
+        for idx, encodes in enumerate(encoder):
+            output, feed = encodes
+            inputs.append(feed)
+            output = self.expand_sentence_tensor(output)
+            if idx == fact_num:
+                # question is always the last tensor
+                question = output
+            else:
+                facts.append(output)
+
+        for layer in range(self.num_reasoning):
+            with tf.variable_scope('reasoning_layer_' + str(layer)) as scope:
+                print question
+                question, facts = self.generate_resaoner(question, facts)
+        return inputs, question
 
     def generate_rnn_layer(self):
         '''
@@ -79,6 +100,8 @@ class MemNet():
     def generate_encoder(self, num_sentence, input_shape):
         '''
             encoding layer to encode facts and question
+            the input_shape should be the shape of:
+                batch * sentence length * word vector length
         '''
         sentence_holder = []
         for _ in range(num_sentence):
@@ -90,16 +113,16 @@ class MemNet():
                                             dtype = tf.float64,
                                             inputs = tmp
                                             )
-                sentence_holder.append((output, last_state, tmp))
+                # only the take the last output as the sentence representation
+                sentence_holder.append((output[:, -1, :], tmp))
         return sentence_holder
 
     def generate_conv_layer(self, question, fact):
-        feed = tf.concat(0, [question, fact])
+        feed = tf.concat(1, [question, fact])
         for i in range(self.depth_cnn):
             with tf.variable_scope('depth_' + str(i)) as scope:
-                print feed.get_shape(), self.filter, self.strides
-                feed = tf.nn.conv2d(feed, filter = self.filter, strides = self.strides, padding = 'VALID')
-        return tf.split(0, 2, feed)
+                feed = tf.nn.conv2d(feed, filter = self.filter, strides = self.strides, padding = 'SAME')
+        return tf.split(1, 2, feed)
 
     def generate_resaoner(self, question, facts):
         qs = []
@@ -109,7 +132,7 @@ class MemNet():
                 q, f = self.generate_conv_layer(question, fact)
                 qs.append(q)
                 fs.append(f)
-        question = tf.concat(0, qs)
+        question = tf.concat(1, qs)
         return (question, fs)
 
 if __name__ == '__main__':
